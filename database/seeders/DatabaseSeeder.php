45555f7ae04767c9cf8b10a4636db3840587fbf9
NadilaHasanah <nandinihasanah@gmail.com>
2023-10-13 11:12:58 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,24 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            'name' => 'Nadila Andini',
-            'email' => 'nandinihasanah@gmail.com',
-            'password' => bcrypt('1228')
-        ]);
+        // User::create([
+        //     'name' => 'Nadila Andini',
+        //     'email' => 'nandinihasanah@gmail.com',
+        //     'password' => bcrypt('1228')
+        // ]);
 
-        User::create([
-            'name' => 'Isnanisa Rasyidah',
-            'email' => 'isnanr@gmail.com',
-            'password' => bcrypt('1258')
-        ]);
+        // User::create([
+        //     'name' => 'Isnanisa Rasyidah',
+        //     'email' => 'isnanr@gmail.com',
+        //     'password' => bcrypt('1258')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Programming',
@@ -44,31 +45,33 @@ class DatabaseSeeder extends Seeder
             'slug' => 'web-design'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos,',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos, reiciendis numquam totam reprehenderit consectetur? Tenetur debitis molestias sit quisquam accusantium minima voluptatibus distinctio excepturi similique. Eveniet illo sed minus veniam laudantium libero vel qui maiores iste placeat minima odio iure excepturi nemo illum totam, dolore ea, laborum fugit et. Nemo dolorem molestias quod quidem quisquam adipisci excepturi, dolores quas recusandae reprehenderit! Modi sed saepe nesciunt in, dolore ut!',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos,',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos, reiciendis numquam totam reprehenderit consectetur? Tenetur debitis molestias sit quisquam accusantium minima voluptatibus distinctio excepturi similique. Eveniet illo sed minus veniam laudantium libero vel qui maiores iste placeat minima odio iure excepturi nemo illum totam, dolore ea, laborum fugit et. Nemo dolorem molestias quod quidem quisquam adipisci excepturi, dolores quas recusandae reprehenderit! Modi sed saepe nesciunt in, dolore ut!',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos,',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos, reiciendis numquam totam reprehenderit consectetur? Tenetur debitis molestias sit quisquam accusantium minima voluptatibus distinctio excepturi similique. Eveniet illo sed minus veniam laudantium libero vel qui maiores iste placeat minima odio iure excepturi nemo illum totam, dolore ea, laborum fugit et. Nemo dolorem molestias quod quidem quisquam adipisci excepturi, dolores quas recusandae reprehenderit! Modi sed saepe nesciunt in, dolore ut!',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos,',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos, reiciendis numquam totam reprehenderit consectetur? Tenetur debitis molestias sit quisquam accusantium minima voluptatibus distinctio excepturi similique. Eveniet illo sed minus veniam laudantium libero vel qui maiores iste placeat minima odio iure excepturi nemo illum totam, dolore ea, laborum fugit et. Nemo dolorem molestias quod quidem quisquam adipisci excepturi, dolores quas recusandae reprehenderit! Modi sed saepe nesciunt in, dolore ut!',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos,',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos, reiciendis numquam totam reprehenderit consectetur? Tenetur debitis molestias sit quisquam accusantium minima voluptatibus distinctio excepturi similique. Eveniet illo sed minus veniam laudantium libero vel qui maiores iste placeat minima odio iure excepturi nemo illum totam, dolore ea, laborum fugit et. Nemo dolorem molestias quod quidem quisquam adipisci excepturi, dolores quas recusandae reprehenderit! Modi sed saepe nesciunt in, dolore ut!',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos,',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ipsa perferendis quam tempora, quis facere voluptatum quas pariatur odio corporis eos, reiciendis numquam totam reprehenderit consectetur? Tenetur debitis molestias sit quisquam accusantium minima voluptatibus distinctio excepturi similique. Eveniet illo sed minus veniam laudantium libero vel qui maiores iste placeat minima odio iure excepturi nemo illum totam, dolore ea, laborum fugit et. Nemo dolorem molestias quod quidem quisquam adipisci excepturi, dolores quas recusandae reprehenderit! Modi sed saepe nesciunt in, dolore ut!',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
+
         <h2 class="mb-5">{{ $post->title }}</h2>
 
-        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a></p>
+        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{ $post->category->name }}</a></p>
         {!!  $post->body  !!}
-    </article>
+
 
     <a href="/blog" class="text-decoration-none mt-3">Back to Posts</a>
 @endsection
